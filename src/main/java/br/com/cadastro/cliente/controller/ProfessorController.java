280f021fe52f0570bcc4ece6f7cb43ea88f08869
pedroluiznogueira <peluiznogueiram@hotmail.com>
2021-10-22 23:22:23 -0300

    Trocando nome do endpoint e organizando os métodos do controller com comentários

--- src/main/java/br/com/cadastro/cliente/controller/ProfessorController.java
+++ src/main/java/br/com/cadastro/cliente/controller/ProfessorController.java
@@ -11,47 +11,54 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/cliente")
+@RequestMapping("/professor")
 @CrossOrigin("*")
 public class ProfessorController {
 
     @Autowired
     private ProfessorService clienteService;
 
-    @PostMapping("pesquisa")
-    public ResponseEntity<List<Cliente>> pesquisar(@RequestBody Cliente cliente) {
-        List<Cliente> clientes = clienteService.pesquisar(cliente.getNome());
-        return new ResponseEntity<List<Cliente>>(clientes, HttpStatus.OK);
-    }
-
-    @GetMapping("pesquisa/{id}")
-    public ResponseEntity<Cliente> findById(@PathVariable ("id") Long id) {
-        Cliente cliente = clienteService.findClienteById(id);
-        return new ResponseEntity<Cliente>(cliente, HttpStatus.OK);
-    }
-
+    // todos professores
     @GetMapping
     public ResponseEntity<List<Cliente>> getClientes() {
         List<Cliente> lista = clienteService.getClientes();
         return new ResponseEntity<List<Cliente>>(lista, HttpStatus.OK);
     }
 
+    // professor por id
+    @GetMapping("pesquisa/{id}")
+    public ResponseEntity<Cliente> findById(@PathVariable ("id") Long id) {
+        Cliente cliente = clienteService.findClienteById(id);
+        return new ResponseEntity<Cliente>(cliente, HttpStatus.OK);
+    }
+
+    // cadastrar professor
     @PostMapping
     public ResponseEntity<StatusResponse> insertCliente(@RequestBody Cliente cliente) {
         StatusResponse statusResponse = clienteService.insertCliente(cliente);
         return new ResponseEntity<StatusResponse>(statusResponse, HttpStatus.OK);
     }
 
+
+    // excluir professor
     @DeleteMapping("/{id}")
     public ResponseEntity<StatusResponse> dropCliente(@PathVariable ("id") Long idCliente) {
         StatusResponse statusResponse = clienteService.dropCliente(idCliente);
         return new ResponseEntity<StatusResponse>(statusResponse, HttpStatus.OK);
     }
 
+    // atualizar professor
     @PutMapping
     public ResponseEntity<StatusResponse> updateCliente(@RequestBody Cliente cliente) {
         StatusResponse statusResponse = clienteService.updateCliente(cliente);
 
         return new ResponseEntity<StatusResponse>(statusResponse, HttpStatus.OK);
+    }
+
+    // fazer pesquisa por professores
+    @PostMapping("pesquisa")
+    public ResponseEntity<List<Cliente>> pesquisar(@RequestBody Cliente cliente) {
+        List<Cliente> clientes = clienteService.pesquisar(cliente.getNome());
+        return new ResponseEntity<List<Cliente>>(clientes, HttpStatus.OK);
     }
 }
